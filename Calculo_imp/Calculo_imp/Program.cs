7e402ffbd0d8c7a55fd7d734de0b6eb9414b86e0
Nelson <nelsourena14@gmail.com>
2022-05-21 00:23:49 -0400

    committing a code file

---
+++ Calculo_imp/Calculo_imp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Calculo_imp
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double vSueldob, neto, imp;
+
+            Console.WriteLine("Ingrese su sueldo: ");
+            vSueldob = Convert.ToInt32(Console.ReadLine());
+
+            double AFP = vSueldob * 0.07, ISR = vSueldob * 0.09, SSF = vSueldob * 0.10;
+
+            imp = AFP + ISR + SSF;
+
+            neto = vSueldob - imp;
+
+            Console.WriteLine("Su sueldo neto es de: " + neto);
+
+
+
+        }
+    }
+}
